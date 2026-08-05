4a227e5e9a7e30d8a0128c200247c870ab3eceb4
Buğra Karkucak <bugrakarkucakk@gmail.com>
2026-08-05 10:51:16 +0300

    Frontend şablonu Blade mimarisine uyarlandı, tüm rotalar bağlandı, link rotaları ve HTML uyarıları temizlendi

--- projects/StajProjesi/routes/web.php
+++ projects/StajProjesi/routes/web.php
@@ -3,15 +3,15 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('welcome');
-})->name('home');
-
-Route::middleware(['auth'])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
-});
+Route::view('/', 'index');
+Route::view('/hakkimizda', 'about');
+Route::view('/iletisim', 'contact');
+Route::view('/sartlar', 'terms');
+Route::view('/blog', 'blog');
+Route::view('/a', 'blog-details');
+Route::view('/b', 'testimonials');
+Route::view('/c', 'vacations');
+Route::view('/d', 'vacation-details');
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
